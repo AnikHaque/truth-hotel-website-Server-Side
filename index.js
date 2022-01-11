e2d3517d2c24d1515831786d76ba2591959934a2
AnikHaque <ekramul15-811@diu.edu.bd>
2022-01-11 15:40:01 +0600

    added

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); 
 
-// user: truth-hotel 
-// pass: RzqrpJQwkfVqFCpo 
-
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lx750.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lx750.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
